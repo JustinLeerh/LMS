4fab035e0a63ab32d2285f75adf9cc737b5c6296
confusednut <wongziyang01@gmail.com>
2022-06-13 18:32:39 +0800

    akjs

--- App.js
+++ App.js
@@ -1,23 +1,22 @@
-
 import React from 'react';
 import { Text, View, StyleSheet, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from "./Screens/HomeScreen"
-import SettingsScreen from "./Screens/SettingScreen"
-import EventScreen from "./Screens/EventScreen"
-
+import HomeScreen from "./Screens/Home"
+import LoginScreen from "./Screens/LoginScreen"
+import EventScreen from "./Screens/ContactScreen"
+import { createStackNavigator } from '@react-navigation/stack'
 
 
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Login" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Login" options={{headerShown: false}} component={LoginScreen} />
         <Tab.Screen name="Event" component={EventScreen} />
       </Tab.Navigator>
     </NavigationContainer>
@@ -31,29 +30,3 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
 });
-
-/*export default function App() {
-
-  function buttonPressed() {
-    console.log("ButtonPressed")
-  }
-
-  return (
-    <View style={styles.container}>
-      <Button title="Login" onPress={buttonPressed}></Button>
-    </View>
-  );
-
-  
-
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});*/
-
-
